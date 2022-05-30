2fcbd0cf72f6e750c390f45f6ba7fe437a89b76e
Aarsh Shah <aarshshah99@gmail.com>
2022-05-30 11:48:46 -0600

    Added solution for problem 5

--- Longest_Palindromic_Substring_5.py
+++ Longest_Palindromic_Substring_5.py
@@ -1,3 +1,44 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        return None
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+#         if len(s) == 1 or not s:
+#             return s
+
+#         test = True
+#         s_array = s.split()
+
+#         while test:
+#             if s_array.reverse() == s_array:
+#                 test = False
+#                 return s_array
+#             else:
+#                 del s_array[-1]
+#                 del s_array[0]
+
+
+
+# def longestPalindrome(s):
+#         if len(s) == 1 or not s:
+#             return s
+
+#         test = True
+#         s_array = s.split()
+
+#         while test:
+#             if s_array.reverse() == s_array:
+#                 test = False
+#                 print(s_array)
+#             else:
+#                 s_array.pop()
+#                 s_array.pop(0)
+
+
+# longestPalindrome('babad')
+
+
+arr = 'aba'
+array = arr.split()
+
+if array.reverse() == array:
+    print('true')
+
+print(array.reverse(), array)
